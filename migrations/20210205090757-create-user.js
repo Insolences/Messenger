4b2e7cc88ba06ab8292f617e2e1b13f3cfb9450c
Yujin <yujinwork19@gmail.com>
2021-02-05 11:14:13 +0200

    initial project and added user model for migrations

---
+++ migrations/20210205090757-create-user.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      login: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique:true
+      },
+      nickname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      is_Admin: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      is_Blocked: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  down: async (queryInterface, DataTypes) => {
+    await queryInterface.dropTable('users');
+  }
+};
