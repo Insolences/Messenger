c9df9446781b655cfcb4413c850b80b653d7c556
Yujin <yujinwork19@gmail.com>
2021-02-08 14:28:46 +0200

    added cors in project

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const db = require("./models");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const HOST = process.env.DB_HOST;
 const PORT = process.env.PORT;
 const authRouter = require("./routes/authRouter");
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(cors());
 
 app.use(`/${process.env.SR_URL}`, authRouter);
 
