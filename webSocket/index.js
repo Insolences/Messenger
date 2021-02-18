5ad6eb9ced18d8d3d62d94afca3b4c4280a904c1
Oleg <roumiatsev.adress@gmail.com>
2021-02-18 10:53:51 +0200

    end

--- webSocket/index.js
+++ webSocket/index.js
@@ -80,15 +80,7 @@ const socketServer = (server) => {
           }))
         )
         .flat();
-      queryMessages.sort(function (a, b) {
-        if (a.id > b.id) {
-          return 1;
-        }
-        if (a.id < b.id) {
-          return -1;
-        }
-        return 0;
-      });
+
       setTimeout(() => {
         socket.emit("getUserInfo", {
           query: {
